b2666bb99781a441384329e78c8930bb86c03963
Luke Boardman <lukejamesboardman@icloud.com>
2025-03-01 20:41:23 -0500

    Fix: made it so multiple radio buttons cant be active at the same time

--- project1starter.php
+++ project1starter.php
@@ -8,65 +8,62 @@
 
 
 <!-- TODO: Fix all bugs/poor practice in the form -->
-<form action="Project2.php" method="post" class="survey">
+<form action="Processing.php" method="post" class="survey">
 
 <div>
     <fieldset>
-        <legend>What is your email?</legend>
-            <label>Enter your email: </label>
+        <legend>Enter your email: </legend>
             <input type="email" name="email-name" id="email-id">
     </fieldset>
 </div>
 
 <div>
     <fieldset>
-        <legend>What is your password?</legend>
-            <label>Enter your password: </label>
+        <legend>Enter your password: </legend>
             <input type="text" name="pw-name" id="pw-id">
     </fieldset>
 </div>
 
 <div>
     <fieldset>
-        <legend>What is your age?</legend>
-        <label>What age are you? </label>
-            <input type="radio" name="0-12" id="0">
+        <legend>What age are you?</legend>
+            <input type="radio" name="age" id="0">
             <label>0-12 </label>
 
-            <input type="radio" name="13-17" id="1">
+            <input type="radio" name="age" id="1">
             <label>13-17 </label>
 
-            <input type="radio" name="18-22" id="2">
+            <input type="radio" name="age" id="2">
             <label>18-22 </label>
 
-            <input type="radio" name="23-27" id="3">
+            <input type="radio" name="age" id="3">
             <label>23-27 </label>
 
-            <input type="radio" name="28-32" id="4">
+            <input type="radio" name="age" id="4">
             <label>28-32 </label>
 
-            <input type="radio" name="33-37" id="5">
+            <input type="radio" name="age" id="5">
             <label>33-37 </label>
 
-            <input type="radio" name="38-42" id="6">
+            <input type="radio" name="age" id="6">
             <label>38-42 </label>
 
-            <input type="radio" name="43-47" id="7">
+            <input type="radio" name="age" id="7">
             <label>43-47 </label>
 
-            <input type="radio" name="48-52" id="8">
+            <input type="radio" name="age" id="8">
             <label>48-52 </label>
 
-            <input type="radio" name="53-57" id="9">
+            <input type="radio" name="age" id="9">
             <label>53-57 </label>
 
-            <input type="radio" name="58-62" id="10">
+            <input type="radio" name="age" id="10">
             <label>58-62 </label>
 
-            <input type="radio" name="63-67" id="11">
+            <input type="radio" name="age" id="11">
             <label>63-67 </label>
 
-            <input type="radio" name="68+" id="12">
+            <input type="radio" name="age" id="12">
             <label>68+ </label>
     </fieldset>
 </div>
@@ -75,17 +72,22 @@
     <fieldset>
         <legend>What is your Gender?</legend>
             <select name="gender" id="gender">
-            <option value="m">Male</option>
-            <option value="f">Female</option>
-            <option value="nb">Nonbinary</option>
-            <option value="gf">Genderfluid</option>
-            <option value="a">Agender</option>
-            <option value="o">Choose not to say/Other</option>
+                <option value="m" name = "Gender">Male</option>
+                <option value="f" name = "Gender">Female</option>
+                <option value="nb" name = "Gender">Nonbinary</option>
+                <option value="gf" name = "Gender">Genderfluid</option>
+                <option value="a" name = "Gender">Agender</option>
+                <option value="o" name = "Gender">Choose not to say/Other</option>
             </select>
     </fieldset>
 </div>
 
-
+<div>
+    <fieldset>
+        <legend>Blargus</legend>
+        
+    </fieldset>
+</div>
 <!-- TODO: Add your own survey questions -->
 <div>
     <button type="submit" name="submit-button" id="submit-button"><h6>Submit</h6></button>
