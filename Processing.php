4c989babd60e49824ccd5812b665b672979bb37d
Luke Boardman <lukejamesboardman@icloud.com>
2025-03-13 17:07:45 -0400

    Feat: moved over updated base code from project 1 for an easier time.

--- Processing.php
+++ Processing.php
@@ -7,86 +7,114 @@
 
 
 
-<form action="project1starter.php" method="post" class="survey-form"></form>
+<form action="SQLTable.php" method="post" class="survey-form"></form>
 
 
 
 <?php
+include 'dbconfig.php';
+$db = connectDB();
 
-
+    //pulling info from Form
     $un_sanitized_email = $_POST["email-name"];
     $un_validated_password = $_POST["pw-name"];
     $un_validated_age = $_POST["age"];
     $un_validated_animal = $_POST["animal"];
+    $un_validated_gender = $_POST["gender"];
+    $un_validated_date = $_POST["year"];
+
+
+    //Sanitize text my converting special characters
+    $un_sanitized_email = htmlspecialchars($un_sanitized_email);
+    $validated_password = htmlspecialchars($un_validated_password);
+    $un_validated_age = htmlspecialchars($un_validated_age);
+
+    //Arrays for genders and animals
+    $validAnimals = ['dog', 'cat', 'both', 'other'];
+    $validGenders = ['male', 'female', 'nonbinary', 'genderfluid', 'agender', 'other'];
+
+        //  filter genders
+    function filter_genders($un_validated_gender) { 
+        global $validGenders;
+        if (in_array($un_validated_gender, $validGenders)) {
+            echo"--This gender is valid--";
+        } else {
+            echo"--This gender is valid--";
+        }
+    }
     
-
-    htmlspecialchars($un_sanitized_email);
-    htmlspecialchars($un_validated_password);
-    htmlspecialchars($un_validated_age);
-    htmlspecialchars($un_validated_animal);
-
-
+        //  filter emails
     function filter_emails ($un_sanitized_email) {
         $sanitized_email = filter_var($un_sanitized_email, FILTER_SANITIZE_EMAIL);
         if (filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
-            echo "  This (a) sanitized email address is considered valid.      ";
+            echo "--This email is valid--";
         } else {
-            echo "  This (b) sanitized email address is considered invalid.      ";
+            echo "--This email is invalid--";
         } 
     }
-    
+        //  filter age
     function filter_age ($un_validated_age) { 
-    if (is_numeric($un_validated_age > 1)) {
+    if (is_numeric($un_validated_age)) {
         $validated_age = $un_validated_age;
-        echo "  This (a) validated age is considered valid.    ";
+        echo "--This age is valid--";
     } else {
-        echo "  This (b) validated age is considered invalid.   ";
+        echo "--This age is invalid--";
     }
 }
-
-    function filter_animal ($un_validated_animal) {
-        if (is_numeric($un_validated_animal)) {
-            $validated_animal = $un_validated_animal;
+        // filter animals
+        function filter_animal($un_validated_animal) { 
+            global $validAnimals;
+            if (in_array($un_validated_animal, $validAnimals)) {
+                echo"--This animal is valid--";
+            } else {
+                echo"--This animal is invalid--";
+            }
         }
-    }
 
+        // filter dates
+        function filter_date ($un_validated_year, $format = 'Y-m-d') { 
+           $date = DateTime::createFromFormat($format, $un_validated_year);
+           $validated_date = $date && $date->format($format) == $un_validated_year;
+           echo '--This date is valid--';
+        }
 
+        //run functions
         filter_age($un_validated_age);
         filter_emails($un_sanitized_email);
         filter_animal($un_validated_animal);
+        filter_genders($un_validated_gender);
+        filter_date('$un_validated_date', 'Y-m-d');
 
     
-include 'dbconfig.php';
-$db = connectDB();
-$password = 'Diaphram';
-$hashString = password_hash($password, PASSWORD_DEFAULT);
-$Userpassword = $_POST['pw-name'];
-echo $Userpassword;
-$verified = password_verify($Userpassword, $hashString);
+
+
+
+//Password verification
+
+$hashString = '$2y$10$.oDyPlmjZC00P6sUIIRnxeD1CBTMPXfH0JshahCEuVsDNDxSSWfRy';
+$Userpassword = $validated_password;        
+
+
+if (password_verify($Userpassword, $hashString)) {
+    echo "--this password is valid--";
+
+        $insert_data = $db->prepare("INSERT INTO Project2SQL (age) VALUES (?);");
+        $insert_data->execute(array($validated_age));
+
+        $insert_data = $db->prepare("INSERT INTO Project2SQL (email) VALUES (?);");
+        $insert_data->execute(array($sanitized_email));
+
+        $insert_data = $db->prepare("INSERT INTO Project2SQL (petType) VALUES (?);");
+        $insert_data->execute(array($validated_animal));
+
+    } else {
+        echo "--This password is invalid--";
+    }
 
 
 
 
-        if ($verified = True) {
-            echo '  Welcome User    ';
-            
-            
-            
-            $insert_data = $db->prepare("INSERT INTO Project2SQL (age) VALUES (?);");
-            $insert_data->execute(array($validated_age));
-    
-            $insert_data = $db->prepare("INSERT INTO Project2SQL (email) VALUES (?);");
-            $insert_data->execute(array($sanitized_email));
-    
-            $insert_data = $db->prepare("INSERT INTO Project2SQL (petType) VALUES (?);");
-            $insert_data->execute(array($validated_animal));
-        } else {
-            echo "  PASSWORD INCORRECT  ";
-        }
-
-
-       
 ?>
-
 </body>
-</html>
+</html>   
+           
--- project1starter.php
+++ project1starter.php
@@ -10,75 +10,72 @@
 
 <form action="Processing.php" method="post" class="survey-form">
 
-<div>
+<div class = "form-group">
     <fieldset>
         <legend>Enter your email: </legend>
-            <input type="email" name="email-name" id="email-id" minlength="5" required>
+            <input class="form-control" type="email" name="email-name" id="email-id" minlength="5" required>
     </fieldset>
 </div>
 
-<div>
+<div class = "form-group">
     <fieldset>
         <legend>Enter your password: </legend>
-            <input type="password" name="pw-name" id="pw-id" minlength="8" required>
+            <input class="form-control" type="password" name="pw-name" id="pw-id" minlength="8" required>
     </fieldset>
 </div>
 
-<div>
+<div class = "form-group">
     <fieldset>
         <legend>What age are you?</legend>
-            <input type="number" name="age" id="age" min="5" max="127" required>
+            <input class="form-control" type="number" name="age" id="age" min="5" max="127" required>
     </fieldset>
 </div>
 
-<div>
+<div class = "form-group">
     <fieldset>
         <legend>What is your Gender?</legend>
             <select name="gender" id="gender">
-                <option value="m" name = "Gender">Male</option>
-                <option value="f" name = "Gender">Female</option>
-                <option value="nb" name = "Gender">Nonbinary</option>
-                <option value="gf" name = "Gender">Genderfluid</option>
-                <option value="a" name = "Gender">Agender</option>
-                <option value="o" name = "Gender">Choose not to say/Other</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="nonbinary">Nonbinary</option>
+                <option value="genderfluid">Genderfluid</option>
+                <option value="agender">Agender</option>
+                <option value="other">Choose not to say/Other</option>
             </select>
     </fieldset>
 </div>
 
-<div>
+<div class = "form-group">
     <fieldset>
         <legend>Do you currently own a dog/cat?</legend>
-        <input type="checkbox" name="animal" id="dog">
+        <input type="checkbox" name="animal" value="dog">
         <label for="dog">Dog</label>
 
-        <input type="checkbox" name="animal" id="cat">
+        <input type="checkbox" name="animal" value="cat">
         <label for="cat">Cat</label>
 
-        <input type="checkbox" name="animal" id="other">
+        <input type="checkbox" name="animal" value="both">
+        <label for="both">Both</label>
+
+        <input type="checkbox" name="animal" value="other">
         <label for="other">Other</label>
     </fieldset>
 </div>
 
-<div>
+<div class = "form-group">
     <fieldset>
         <legend>What year did you get your pet?</legend>
-        <input type="month" name="date" id="date" min="2000-01" >
+        <input type="date" name="year" id="year" min="2000-01-01" max="2025-3-31">
     </fieldset>
 </div>
 
 
-<div>
+<div class = "form-group">
     <button type="submit" name="submit-button" id="submit-button"><h6>Submit</h6></button>
 </div>
 
 </form>
 
-<?php 
-include 'dbconfig.php';
-
-
-
-?>
 
 <!-- TODO: All the backend PHP/SQL stuff! (you may need a separate file for this!) -->
 
